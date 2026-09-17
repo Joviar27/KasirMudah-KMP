093e6c03f9d99c943ca1d1a63f33a052e9e20d64
noobmobdev <noobmobdev@gmail.com>
2026-09-17 16:43:10 +0700

    feat: setup Kermit logging

--- androidApp/src/main/kotlin/com/cobasendiri/kasirmudahkmp/MainApplication.kt
+++ androidApp/src/main/kotlin/com/cobasendiri/kasirmudahkmp/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.cobasendiri.kasirmudahkmp
 
 import android.app.Application
+import co.touchlab.kermit.LogcatWriter
+import co.touchlab.kermit.Logger
+import co.touchlab.kermit.Severity
 import com.cobasendiri.kasirmudahkmp.di.initKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -11,6 +14,13 @@ class MainApplication: Application() {
         initKoin{
             androidLogger()
             androidContext(this@MainApplication)
+        }
+
+        if(BuildConfig.DEBUG){
+            Logger.setMinSeverity(Severity.Debug)
+            Logger.setLogWriters(LogcatWriter())
+        }else{
+            Logger.setLogWriters(emptyList())
         }
     }
 }
